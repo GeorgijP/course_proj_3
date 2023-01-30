315c0eed636ce44f9624d34ea4af4e4155a4ecce
GeorgijP <georgij.puzanov@inbox.ru>
2023-01-30 19:09:50 +0400

    Доработана функция open_list_oprations (Тепеь еще Формирует список выполненных переводов)

--- utils.py
+++ utils.py
@@ -1,8 +1,18 @@
 import requests
 def open_list_operations():
     """
-    Получает данные о переводах
+    Получает данные о переводах.
+    Формирует список выполненных переводов
     """
-    list_operations = requests.request("GET", "https://jsonkeeper.com/b/0MZI", verify=False)
-    list_operations = list_operations.json()
-    return list_operations
+    executed_operations = []
+
+    list_operations = requests.request("GET", "https://jsonkeeper.com/b/0MZI", verify=False).json()
+
+    for i in range(len(list_operations)):
+        if 'state' not in list_operations[i]:
+            continue
+        if list_operations[i]['state'] == 'EXECUTED':
+            executed_operations.append(list_operations[i])
+
+    return executed_operations
+
